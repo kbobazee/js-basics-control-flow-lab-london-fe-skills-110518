7b494ef25497020852676937b5fe6a4233299896
Khloe Bailey Obazee <khloe.obazee@wework.com>
2018-12-17 17:44:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(freeride) {
+  if (freeride <=400) {
+    return 'This one is on me!'
+    
+  } else if (freeride >2000) {
+    return 'I will gladly take your thirty bucks.'
+    
+  } if (freeride <2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  
+  function scuberGreetingForFeet(longride) {
+  if (longride =>2501) {
+    return 'No can do.'}
+  }
+}
+
+function ternaryCheckCity(NYC) {
+  if (NYC <='NYC') {
+    return 'Ok, sounds good.'
+    
+  } else if (NYC ='pittsburg') {
+    return 'No go.'}
+    
+  }
+
+
+
+
+
